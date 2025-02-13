9632cd3ccc3cceb49d99c2d8a05f95476f30da46
Aj Wazzan <x_1337@outlook.com>
2025-02-13 14:27:48 -0800

    chore: disable IP tracking in authentication configuration

--- lib/auth.ts
+++ lib/auth.ts
@@ -19,6 +19,11 @@ const options = {
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
+  advanced: {
+    ipAddress: {
+      disableIpTracking: true,
+    },
+  },
   session: {
     expiresIn: 60 * 60 * 24 * 7, // 7 days
     updateAge: 60 * 60 * 24, // 1 day (every 1 day the session expiration is updated)
