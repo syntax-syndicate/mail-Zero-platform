f3a3a06919216cc37c7716807846f8bea87742ef
Nizzy <nizabizaher@gmail.com>
2025-02-21 00:53:02 -0500

    homepage

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,12 +1,25 @@
 "use client";
 
 import { GitHub, Google } from "@/components/icons/icons";
+import { signIn, useSession } from "@/lib/auth-client";
 import { Button } from "@/components/ui/button";
-import { signIn } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import Image from "next/image";
 import { toast } from "sonner";
 
 export default function Login() {
+  const router = useRouter();
+  const { data: session, isPending } = useSession();
+
+  useEffect(() => {
+    if (!isPending && session?.connectionId) {
+      router.push("/mail");
+    }
+  }, [session, isPending, router]);
+
+  if (isPending || (session && session.connectionId)) return null;
+
   return (
     <div className="flex min-h-screen w-full items-center justify-center bg-black">
       <div className="max-w-[500px] space-y-8 px-4 duration-500 animate-in slide-in-from-bottom-4 sm:px-12 md:px-0">
