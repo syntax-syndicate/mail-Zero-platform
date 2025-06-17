40adcbdd3a00c78a6823f8fd9c3021cd7e2efd63
Manik Sharma <115614705+maniksharma424@users.noreply.github.com>
2025-06-17 23:29:21 +0530

    fixed clear button alingment issue in inbox search tab (#1332)

--- apps/mail/components/mail/mail.tsx
+++ apps/mail/components/mail/mail.tsx
@@ -549,7 +549,7 @@ export function MailLayout() {
                       : 'Search...'}
                   </span>
 
-                  <span className="absolute right-[0.1rem] flex gap-1">
+                  <span className="absolute right-[0.1rem] flex gap-1  items-center">
                     {/* {activeFilters.length > 0 && (
                       <Badge variant="secondary" className="ml-2 h-5 rounded px-1">
                         {activeFilters.length}
